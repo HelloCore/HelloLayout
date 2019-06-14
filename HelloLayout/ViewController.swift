fe3ed67ff96133d3b39e979b609d29e5ac38114c
Benz <aukwat@gmail.com>
2019-06-14 14:21:06 +0700

    Initialize Project

---
+++ HelloLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloLayout
+//
+//  Created by Akkharawat Chayapiwat on 14/6/2562 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
